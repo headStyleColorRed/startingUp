825ff28eb6d51e095b284c7161568b6857345de8
Rodrigo Labrador <headstylecolorred@gmail.com>
2020-06-21 02:26:41 +0200

    added strings and began login

---
+++ src/components/loginForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="loginForm">
+	       <div class="inputEmail">
+          <p>Name</p>
+		  <input class="inputFields" type="text" v-model="user.email">
+		  <input type="text" id="fname" name="fname">
+      </div>
+      <div class="inputPassword"></div>
+      <div v-if="!isShowingLogin" class="inputConfirmPassword"></div>
+  </div>
+</template>
+
+<script>
+import stringsImport from "../assets/Strings_en.json";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      isShowingLogin: true,
+      Strings: new Object()
+    };
+  },
+  mounted() {
+    this.Strings = stringsImport.screen_login;
+  },
+  methods: {
+    changeLoginScreen() {
+      this.isShowingLogin = !this.isShowingLogin;
+    }
+  },
+  components: {}
+};
+</script>
+
+<style scoped>
+.loginForm {
+	border: 1px solid black;
+	height: 25vw;
+	width: 30rem;
+}
+
+.inputFields {
+	background-color: rgb(114, 114, 114);
+}
+
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,107 @@
 <template>
   <div class="home">
-	  <h1>Comenzamos Web</h1>
+    <div class="leftContainer">
+		<!--- - - left container top - - - -->
+      <div class="leftContainer-top">
+        <p v-if="isShowingLogin">
+          {{Strings.alreadyAMember}}
+          <b
+            class="header_login"
+            v-on:click="changeLoginScreen()"
+          >{{Strings.signIn}}</b>
+        </p>
+        <p v-else>
+          {{Strings.notAMemberYet}}
+          <b
+            class="header_login"
+            v-on:click="changeLoginScreen()"
+          >{{Strings.signUp}}</b>
+        </p>
+      </div>
+		<!--- - - left container medium - - - -->
+      <div class="leftContainer-medium">
+		  <h2 class="leftContainer-medium-title">{{Strings.signUp}}</h2>
+		  <login-form/>
+	  </div>
+      <div class="leftContainer-bottom"></div>
+    </div>
+    <div class="rightContainer"></div>
   </div>
 </template>
 
 <script>
+import stringsImport from "../assets/Strings_en.json";
+import loginForm from "../components/loginForm.vue"
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      isShowingLogin: true,
+      Strings: new Object()
+    };
+  },
+  mounted() {
+    this.Strings = stringsImport.screen_login;
+  },
+  methods: {
+    changeLoginScreen() {
+      this.isShowingLogin = !this.isShowingLogin;
+    }
+  },
   components: {
+	  "login-form": loginForm,
   }
-}
+};
 </script>
 
 <style scoped>
 .home {
-	height: 100vh;
-	width: 100vw;
-	background-color: rgb(65, 65, 65);
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 1.25fr 1fr;
+}
+
+/* - - - - - - - - L E F T   C O N T A I N E R - - - - - */
+.leftContainer {
+  position: relative;
+  background-color: rgb(255, 255, 255);
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 1fr 4fr 1fr;
+}
+/* - - - left container top - - - */
+.leftContainer-top p {
+  padding: 2rem;
+  text-align: right;
+}
+
+.header_login {
+  font-weight: 400;
+  color: rgb(61, 110, 5);
+  cursor: pointer;
+}
+
+/* - - - left container medium - - - */
+.leftContainer-medium {
+	position: relative;
+	padding: 0 10rem;
+}
+
+.leftContainer-medium-title {
+	font-size: 2.5rem;
+	font-weight: 300;
+	margin-bottom: 2rem;
+}
+
+.leftContainer-bottom {
+  background-color: rgb(77, 139, 35);
+}
+
+/* - - - - - - - R I G H T   C O N T A I N E R - - - - - */
+.rightContainer {
+  background-color: rgb(235, 240, 240);
+  height: 100vh;
 }
 </style>
