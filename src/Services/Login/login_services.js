bc903c0ac001c384bc393f6bd30f1729135b42bf
Rodrigo Labrador <headstylecolorred@gmail.com>
2020-06-24 22:59:14 +0200

    added environments

--- src/Services/Login/login_services.js
+++ src/Services/Login/login_services.js
@@ -1,5 +1,14 @@
 import axios from "axios"
 
+const Environment = {
+	local: "localhost",
+	dev: "15.236.191.15",
+	pro: ""
+}
+
+let currentEnvironment = Environment.dev
+
+
 async function logUser(username, password) {
     let user ={
         "username": username,
@@ -7,7 +16,7 @@ async function logUser(username, password) {
 	}
 	
 	let promise = new Promise((resolve, reject) => {
-		axios.post("http://localhost:8888/login/log_user", user)
+		axios.post(`http://${currentEnvironment}:8888/login/log_user`, user)
         .then((res) => {  
 			resolve(res.data)})
 		.catch((res) => {
@@ -26,7 +35,7 @@ async function registerUser(username, password, passwordConfirmation) {
 	}
 	
 	let promise = new Promise((resolve, reject) => {
-		axios.post("http://localhost:8888/register/register_user", user)
+		axios.post(`http://${currentEnvironment}:8888/register/register_user`, user)
         .then((res) => {  
 			resolve(res.data)})
 		.catch((res) => {
@@ -44,7 +53,7 @@ async function logOut(username, password) {
 	}
 	
 	let promise = new Promise((resolve, reject) => {
-		axios.post("http://localhost:8888/logout/logout_user", user)
+		axios.post(`http://${currentEnvironment}:8888/logout/logout_user`, user)
         .then((res) => {  
 			resolve(res.data)})
 		.catch((res) => {
